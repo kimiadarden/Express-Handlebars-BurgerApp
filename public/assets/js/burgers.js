94492075207f0eabc894584b5605b6c5ee261fd9
Kimia Darden <kimiadarden@gmail.com>
2020-03-03 09:57:33 -0500

    adding public js file

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-sleep").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      var newSleepState = {
+        sleepy: true
+  
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newSleepState
+      }).then(
+        function() {
+          // console.log("changed sleep to", newSleep);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newburger = {
+        name: $("#ca").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newburger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  });
+  
